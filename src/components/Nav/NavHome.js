34fee0a44fac16dbdcf8046ce66c68f7d9dfb4e3
jnicolon <jnicolon@gmail.com>
2022-01-07 17:21:20 -0500

    image at wordpage

--- src/components/Nav/NavHome.js
+++ src/components/Nav/NavHome.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
-import Trending from "./Trending";
 import AlphabetMenu from "./AlphabetMenu";
 import { Link } from "react-router-dom";
 
@@ -13,7 +12,6 @@ function NavHome() {
           <Logo />
         </Link>
         <SearchBar />
-        <Trending />
       </div>
       <AlphabetMenu />
     </div>
--- src/components/WordPage/Word.js
+++ src/components/WordPage/Word.js
@@ -4,6 +4,11 @@ function Word({ wordObject }) {
   console.log(wordObject);
   return (
     <div>
+      <img
+        className="word-page-image"
+        src={`${wordObject.imagePath}`}
+        alt={`${wordObject.word}`}
+      />
       <h1 className="word-page-title">
         {wordObject.word && `${wordObject.word}`}
       </h1>
