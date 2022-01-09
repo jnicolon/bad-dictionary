2fb1406875dbef527e2c96243de2886bc64da465
jnicolon <jnicolon@gmail.com>
2022-01-09 00:52:00 -0500

    link to addword

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Highlights from "../components/Home/Highlights";
 import HomeContent from "../components/Home/HomeContent";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="main-content-container">
       <Highlights />
       <HomeContent />
+      <Link to="/admin/addword">Go to add word</Link>
     </div>
   );
 }
